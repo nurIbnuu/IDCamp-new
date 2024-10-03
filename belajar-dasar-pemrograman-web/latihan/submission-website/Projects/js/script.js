b90535a5cd28cf32be2aa3944858719eb4d58d67
Muhammad Nur Ibnu Hubab <azmutte@gmail.com>
2024-10-03 16:34:17 +0700

    Dark Mode

--- belajar-dasar-pemrograman-web/latihan/submission-website/Projects/js/script.js
+++ belajar-dasar-pemrograman-web/latihan/submission-website/Projects/js/script.js
@@ -20,3 +20,26 @@ document.addEventListener('click', function(e)
     close.classList.remove('bi-x-lg'); 
   };
 });
+
+
+
+// => Theme
+let darkMode = localStorage.getItem('dark-mode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkMode = () => {
+  document.body.classList.add('dark-mode');
+  localStorage.setItem('dark-mode', 'active');
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('dark-mode');
+  localStorage.setItem('dark-mode', null);
+}
+
+if(darkMode === 'active') enableDarkMode();
+
+themeSwitch.addEventListener('click', () => {
+  darkMode = localStorage.getItem('dark-mode');
+  darkMode !== 'active' ? enableDarkMode() : disableDarkMode();
+});
