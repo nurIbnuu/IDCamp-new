fe0e834f12e306bfe7845c6de06c88353bf1e6e2
Muhammad Nur Ibnu Hubab <azmutte@gmail.com>
2024-10-04 14:30:27 +0700

    Merapikan kode - 2

--- belajar-dasar-pemrograman-web/latihan/submission-website/Projects/js/script.js
+++ belajar-dasar-pemrograman-web/latihan/submission-website/Projects/js/script.js
@@ -1,11 +1,10 @@
+// === Toggle Button ===
 const list = document.getElementById('list');
-const navLinks = document.getElementById('navLinks');
+const navLinks = document.getElementById('nav-links');
 const close = document.getElementById('close');
 
-// Ganti Icon ketika tombol List dikllik
 changeIcon = (icon) => icon.classList.toggle('bi-x-lg');
 
-// Jika Icon List diklik, maka tambahkan kelas .active di .nav-links
 list.onclick = function()
 {
   navLinks.classList.toggle('active');
@@ -13,33 +12,37 @@ list.onclick = function()
 
 document.addEventListener('click', function(e)
 {
-  if(!list.contains(e.target) && e.target.id !== 'navLinks')
+  if(!list.contains(e.target) && e.target.id !== 'nav-links')
   {
     navLinks.classList.remove('active');
-    // Jika Navbar sudah tidak aktif, maka kembalikan Icon Close ke List
     close.classList.remove('bi-x-lg'); 
   };
 });
 
 
 
-// => Theme
+
+
+// === Switch Theme ===
 let darkMode = localStorage.getItem('dark-mode');
 const themeSwitch = document.getElementById('theme-switch');
 
-const enableDarkMode = () => {
+const enableDarkMode = () => 
+{
   document.body.classList.add('dark-mode');
   localStorage.setItem('dark-mode', 'active');
-}
+};
 
-const disableDarkMode = () => {
+const disableDarkMode = () =>
+{
   document.body.classList.remove('dark-mode');
   localStorage.setItem('dark-mode', null);
-}
+};
 
 if(darkMode === 'active') enableDarkMode();
 
-themeSwitch.addEventListener('click', () => {
+themeSwitch.addEventListener('click', () =>
+{
   darkMode = localStorage.getItem('dark-mode');
   darkMode !== 'active' ? enableDarkMode() : disableDarkMode();
 });
