a052108a44fadf8dead48fe8fdd5517d01fd247a
Muhammad Nur Ibnu Hubab <azmutte@gmail.com>
2024-10-25 20:19:38 +0700

    Latihan & Murojaah

--- belajar-dasar-pemrograman-javascript/exercises/app.js
+++ belajar-dasar-pemrograman-javascript/exercises/app.js
@@ -17,29 +17,92 @@
 
 
 // Latihan Variabel
-const gender = 'Laki-laki';
-let job = 'Pelajar';
-job = 'Bekerja';
+// const gender = 'Laki-laki';
+// let job = 'Pelajar';
+// job = 'Bekerja';
 
 // Latihan 4: Konversi Tipe Data
-let number = 123;
-let strNumber = String(number);
-console.log(typeof strNumber);
-let strBoolean = 'true';
-let boolean = Boolean(strBoolean);
-console.log(typeof boolean);
+// let number = 123;
+// let strNumber = String(number);
+// console.log(typeof strNumber);
+// let strBoolean = 'true';
+// let boolean = Boolean(strBoolean);
+// console.log(typeof boolean);
 
-console.log('20' + 5);
-console.log('10' * 2);
+// console.log('20' + 5);
+// console.log('10' * 2);
 
 
 // Latihan 5: Operator
-console.log(2 + 3);
-console.log(2 - 3);
-console.log(2 * 3);
-console.log(2 / 3);
+// console.log(2 + 3);
+// console.log(2 - 3);
+// console.log(2 * 3);
+// console.log(2 / 3);
 
-console.log(5 < 7);
+// console.log(5 < 7);
 
-let lima = 5, tujuh = 7;
-console.log((lima < tujuh) ? 'Lima lebih kecil dari Tujuh' : 'Lima tidak lebih kecil dari Tujuh');
+// let lima = 5, tujuh = 7;
+// console.log((lima < tujuh) ? 'Lima lebih kecil dari Tujuh' : 'Lima tidak lebih kecil dari Tujuh');
+
+
+// ---------
+// Jum'at, 25 Oktober 2024
+// Latihan 1: Membuat Function
+
+// Buatlah function yang mengonversi suhu dari Fahrenheit ke Celsius. Gunakan rumus konversi berikut: Celsius=(Fahrenheit−32)×59Celsius=9(Fahrenheit−32)×5​
+// function convertFahrenheitToCelsius(farenheit) {
+//   return (farenheit - 32) * 5 / 9;
+// }
+// console.log(convertFahrenheitToCelsius(212));
+
+// Latihan 2: Function dengan Parameter Default
+
+// Modifikasi function dari Latihan 1 agar memiliki parameter default untuk suhu Fahrenheit. Misalnya, jika tidak ada nilai yang diberikan saat pemanggilan, suhu default adalah 32.
+// function convertFahrenheitToCelsius(fahrenheit = 32) {
+//   return (fahrenheit - 32) * 5 / 9;
+// }
+// console.log(convertFahrenheitToCelsius());
+
+
+// Latihan 3: Function Expression
+
+// Buatlah function expression untuk menghitung luas persegi panjang. Fungsi ini harus menerima dua parameter, panjang dan lebar, dan mengembalikan luasnya.
+
+// const luasPersegiPanjang = function(panjang, lebar) {
+//   return panjang * lebar;
+// }
+// console.log(luasPersegiPanjang(10, 10));
+
+// Latihan 4: Arrow Function
+
+// Ubah function dari Latihan 3 menjadi arrow function. Pastikan tetap berfungsi dengan baik.
+// const luasPersegiPanjang = (panjang, lebar) => panjang * lebar;
+// console.log(luasPersegiPanjang(20, 10));
+
+// const convertFahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * 5 / 9;
+// console.log(convertFahrenheitToCelsius(32));
+
+
+// Latihan 5: Function dengan Return Value
+
+// Buatlah function yang menerima dua angka dan mengembalikan hasil penjumlahan kedua angka tersebut. Nama function-nya jumlah.
+
+// Contoh pemanggilan function:
+
+// const add = (number1, number2) => number1 + number2;
+// console.log(add(100, 300));
+
+
+// Latihan 6: Menggunakan Function sebagai Argumen
+
+// Buatlah function yang menerima dua angka dan satu function sebagai parameter. Function ini harus menggunakan function yang diterima untuk melakukan operasi (misalnya penjumlahan, pengurangan, dll.) pada kedua angka.
+
+// const add = (n1, n2) => n1 + n2;
+// const subtract = (n1, n2) => n1 - n2;
+
+// const calculator = function(operation, n1, n2) {
+//   return operation(n1, n2);
+// }
+
+// console.log(calculator(add, 3, 2));
+// console.log(calculator(subtract, 3, 2));
